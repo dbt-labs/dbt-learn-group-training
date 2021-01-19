7a5d139492b167abd43c3bedf6547c32baf5aa8c
coapacetic <kyle.d.coapman@gmail.com>
2021-01-19 17:54:31 -0500

    fundamentals working session

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,11 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment
