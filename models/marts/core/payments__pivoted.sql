abb3fd676dda47e60a8a6f81e93f12496037d18b
Kyle Coapman <kcoapman@fishtownanalytics.com>
2021-01-20 22:03:30 +0000

    fix pivot

--- models/marts/core/payments__pivoted.sql
+++ models/marts/core/payments__pivoted.sql
@@ -1,4 +1,4 @@
-{% set payment_methods = dbt_utils.get_column_values(table=ref('stg_payments'), column='payment_method']) %}
+{% set payment_methods = dbt_utils.get_column_values(table=ref('stg_payments'), column='payment_method') %}
 
 with payments as (
     select * from {{ ref('stg_payments') }}
@@ -8,7 +8,7 @@ pivoted as (
     select
         order_id,
 
-        {% for payment_method in payment_methods}
+        {% for payment_method in payment_methods %}
 
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
         
@@ -17,10 +17,10 @@ pivoted as (
         {% endif %}
         
         {% endfor %}
-        -- how to handle trailing comma? (if we remove the last column)
-
     from payments
 
     group by 1
 
 )
+
+select * from pivoted
