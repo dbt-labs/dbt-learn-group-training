8b2228ba31a75114794304074f1c9cb8b3dc51a5
Kyle Coapman <kcoapman@fishtownanalytics.com>
2021-06-17 15:47:42 +0000

    tweak orders model

--- models/marts/core/orders.sql
+++ models/marts/core/orders.sql
@@ -9,7 +9,7 @@ payments as (
 order_payments as (
     select
         order_id,
-        sum(case when status = 'success' then amount end) as amount
+        sum(case when status = 'success' then amount end) as amount_usd
 
     from payments
     group by 1
@@ -21,7 +21,7 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
+        coalesce(order_payments.amount_usd, 0) as amount
 
     from orders
     left join order_payments using (order_id)
