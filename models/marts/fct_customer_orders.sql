c3326ff3eb80bff04482fdc7b9aec7017e4a3354
Pat Kearns <pat.kearns@fishtownanalytics.com>
2021-08-11 01:48:05 +0000

    simplify with more window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -47,17 +47,6 @@ paid_orders as (
     left join customers on orders.user_id = customers.id
 ),
 
-customer_orders as (
-    select 
-        customers.id as customer_id
-        , min(orders.order_date) as first_order_date
-        , max(orders.order_date) as most_recent_order_date
-        , count(orders.id) as number_of_orders
-    from customers
-    left join orders on orders.user_id = customers.id 
-    group by 1
-),
-
 -- Final CTE
 
 final as (
@@ -78,10 +67,15 @@ final as (
         row_number() over (partition by paid_orders.customer_id order by paid_orders.order_id) as customer_sales_seq,
         
         -- new vs returning customer
-        case when customer_orders.first_order_date = paid_orders.order_placed_at
-        then 'new'
+        case 
+            when (
+            rank() over (
+                partition by customer_id
+                order by order_placed_at, order_id
+                ) = 1
+            ) then 'new'
         else 'return' end as nvsr,
-        
+
         -- customer lifetime value
         sum(paid_orders.total_amount_paid) over (
             partition by paid_orders.customer_id
@@ -89,12 +83,14 @@ final as (
             ) as customer_lifetime_value,
 
         -- first day of sale
-        customer_orders.first_order_date as fdos
+        first_value(paid_orders.order_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+            ) as fdos
     from paid_orders
-    left join customer_orders on paid_orders.customer_id = customer_orders.customer_id
-    order by order_id
 )
 
 -- Simple Select Statment
 
 select * from final
+order by order_id
