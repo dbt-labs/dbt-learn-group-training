3ee3bfcea09b97e118ba9ab370fd1783fa708166
Kyle Coapman <kcoapman@fishtownanalytics.com>
2021-11-23 16:58:07 +0000

    fix fishtown analytics to dbt-labs in packages.yml

--- dbt_packages/snowflake_spend/analysis/periscope_dashboards/month_to_date.sql
+++
@@ -1,3 +0,0 @@
-SELECT sum(dollars_spent) AS dollars_spend
-FROM  {{ref('snowflake_warehouse_metering_xf')}}
-WHERE  usage_month = date_trunc('month', CURRENT_TIMESTAMP)::date
--- dbt_packages/snowflake_spend/analysis/periscope_dashboards/year_to_date.sql
+++
@@ -1,3 +0,0 @@
-SELECT sum(dollars_spent) AS dollars_spent
-FROM {{ref('snowflake_warehouse_metering_xf')}}
-WHERE date_trunc('year', usage_month) = date_trunc('year', CURRENT_TIMESTAMP)::date
