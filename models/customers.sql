5141c6b4dd7c03976fdc150c882327156a1916d6
coapacetic <kyle.d.coapman@gmail.com>
2022-05-10 09:38:58 -0400

    revert customers model

--- models/customers.sql
+++ models/customers.sql
@@ -1,12 +1,23 @@
 with customers as (
 
-    select * from {{ ref('stg_customers') }}
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
 
 ),
 
 orders as (
 
-   select * from {{ ref('stg_orders') }}
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from raw.jaffle_shop.orders
 
 ),
 
@@ -24,7 +35,6 @@ customer_orders as (
     group by 1
 
 ),
-
 
 final as (
 
