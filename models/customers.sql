fbc087785776f03c1a1b28fff320f12bb5cd650d
Kyle Coapman <kcoapman@dbtlabs.com>
2023-04-26 12:06:11 +0000

    reset to start at the beginning of data modeling

--- models/customers.sql
+++ models/customers.sql
@@ -1,11 +1,21 @@
 with customers as (
 
-    select * from {{ ref('stg_customers') }}),
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
 
+    from raw.jaffle_shop.orders
 ),
 
 customer_orders as (
