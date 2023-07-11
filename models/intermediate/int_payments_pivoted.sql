aaba1dd433b0e5df3e519a156ad187bf3b809d05
Dan Horner <dan.horner@fishtownanalytics.com>
2023-07-11 21:00:26 +0000

    added stg_payments and int_payments_pivoted so that this branch doesn't require so much pre-work before presenting.

---
+++ models/intermediate/int_payments_pivoted.sql
@@ -0,0 +1,22 @@
+with payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+pivoted as (
+    select
+        order_id,
+
+        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
+        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
+        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
+        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount,
+
+        sum(amount) as total
+
+    from payments
+
+    group by 1
+
+)
+
+select * from pivoted
