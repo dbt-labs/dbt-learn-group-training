dfb5d1e99644c657ccf1de0d6901ebf5cf56451d
Bethany Hipple <bethany.hipple@fishtownanalytics.com>
2023-08-14 21:43:03 +0000

    add _etl_loaded_at

--- models/customers.sql
+++ models/customers.sql
@@ -15,7 +15,8 @@ orders as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,
+        _etl_loaded_at
 
     from raw.jaffle_shop.orders
 
