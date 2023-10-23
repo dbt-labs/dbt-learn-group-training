7fd4c6d710cdc5e02db9f65721f6e123d4ef3013
Kyle Coapman <kcoapman@fishtownanalytics.com>
2023-10-23 15:19:48 +0000

    day1_fb

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_jaffle_shop_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status,
-        _etl_loaded_at
-
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop_orders') }}
 
 ),
 
---
+++ models/staging/stg_jaffle_shop_orders.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from raw.jaffle_shop.orders
+
+),
+
+staged as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _etl_loaded_at
+
+    from source
+    
+)
+
+select * from staged
